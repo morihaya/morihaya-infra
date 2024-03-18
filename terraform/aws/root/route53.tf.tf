af641d6603b4b904371abd1321a5f493d5a93a1d
morihaya aka Yukiya Hayashi <ast-hayashi-yukiya@aeonst.onmicrosoft.com>
2024-03-18 11:19:51 +0900

    feat: Add Route53 TXT record for Google Postmaster Tools
    Google postmaster toolsのTXTレコードを追加。
    独自ドメインでメール送信は行なっていないが、可視化＆検証用に。

--- terraform/aws/root/route53.tf.tf
+++ terraform/aws/root/route53.tf.tf
@@ -6,3 +6,17 @@ resource "aws_route53_zone" "morihaya-tech" {
   tags              = {}
   tags_all          = {}
 }
+
+
+# Route53 Txt Records
+
+## Google Postmaster Tools
+resource "aws_route53_record" "googole_postmaster_tools" {
+  zone_id = aws_route53_zone.morihaya-tech.zone_id
+  name    = ""
+  type    = "TXT"
+  ttl     = 300
+  records = [
+    "google-site-verification=KrWd-ES4dWt0_LSLo8F_-oX7ZnclQesj17VEViVP_Wo"
+  ]
+}
