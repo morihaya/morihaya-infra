51c8f8402eabc65638fbdf61138de19427f689a6
Yukiya Hayashi A.k.a morihaya <yukiya.hayashi@gmail.com>
2025-06-29 17:28:00 +0900

    add: Add Route53 A record for example3.morihaya.tech

--- terraform/aws/common-r53/r53.tf
+++ terraform/aws/common-r53/r53.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "example" {
     create_before_destroy = true
   }
 }
+
+# a record for example.morihaya.tech
+resource "aws_route53_record" "example3" {
+  zone_id = data.aws_route53_zone.morihaya_tech.zone_id
+  name    = "example3.morihaya.tech"
+  type    = "A"
+  ttl     = 299
+  records = ["192.0.2.3"]
+  lifecycle {
+    create_before_destroy = true
+  }
+}
