5b510836129d76b4b08b1390dc0b05935ea3fa23
Yukiya Hayashi A.k.a morihaya <yukiya.hayashi@gmail.com>
2026-01-02 14:10:26 +0900

    doc: Update comments for home.morihaya.tech DNS records

--- terraform/aws/common-r53/home.morihaya.tech.tf
+++ terraform/aws/common-r53/home.morihaya.tech.tf
@@ -1,4 +1,6 @@
-# a record for example.morihaya.tech
+# Records for home.morihaya.tech
+# 自宅サーバー関連のDNSレコード。ゾーン切り出しするほどでもないため手動で .home サブドメインに追加。
+
 resource "aws_route53_record" "pve_home" {
   zone_id = data.aws_route53_zone.morihaya_tech.zone_id
   name    = "pve.home.morihaya.tech"
