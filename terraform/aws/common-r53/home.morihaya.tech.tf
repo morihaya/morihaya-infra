5b2a3a3408c7c758c312fdf01f40c62a53fbabea
Yukiya Hayashi A.k.a morihaya <yukiya.hayashi@gmail.com>
2026-01-02 16:06:59 +0900

    feat: Refactor Route53 records into modules for better management and scalability

--- terraform/aws/common-r53/home.morihaya.tech.tf
+++ terraform/aws/common-r53/home.morihaya.tech.tf
@@ -1,35 +1,30 @@
 # Records for home.morihaya.tech
 # 自宅サーバー関連のDNSレコード。ゾーン切り出しするほどでもないため手動で .home サブドメインに追加。
 
-resource "aws_route53_record" "pve_home" {
-  zone_id = data.aws_route53_zone.morihaya_tech.zone_id
-  name    = "pve.home.morihaya.tech"
-  type    = "A"
-  ttl     = 300
-  records = ["192.168.1.2"]
-  lifecycle {
-    create_before_destroy = true
+locals {
+  home_records = {
+    pve = {
+      name    = "pve.home.morihaya.tech"
+      records = ["192.168.1.2"]
+    }
+    ceph = {
+      name    = "ceph.home.morihaya.tech"
+      records = ["192.168.1.3"]
+    }
+    dns = {
+      name    = "dns.home.morihaya.tech"
+      records = ["192.168.1.4"]
+    }
   }
 }
 
-resource "aws_route53_record" "ceph_home" {
-  zone_id = data.aws_route53_zone.morihaya_tech.zone_id
-  name    = "ceph.home.morihaya.tech"
-  type    = "A"
-  ttl     = 300
-  records = ["192.168.1.3"]
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+module "home_records" {
+  source   = "./modules/route53_record"
+  for_each = local.home_records
 
-resource "aws_route53_record" "dns_home" {
   zone_id = data.aws_route53_zone.morihaya_tech.zone_id
-  name    = "dns.home.morihaya.tech"
-  type    = "A"
-  ttl     = 300
-  records = ["192.168.1.4"]
-  lifecycle {
-    create_before_destroy = true
-  }
+  name    = each.value.name
+  type    = lookup(each.value, "type", "A")
+  ttl     = lookup(each.value, "ttl", 300)
+  records = each.value.records
 }
---
+++ terraform/aws/common-r53/modules/route53_record/variables.tf
@@ -0,0 +1,26 @@
+variable "zone_id" {
+  description = "Route 53 Zone ID"
+  type        = string
+}
+
+variable "name" {
+  description = "DNS record name (FQDN)"
+  type        = string
+}
+
+variable "type" {
+  description = "DNS record type (A, CNAME, etc.)"
+  type        = string
+  default = "A"
+}
+
+variable "ttl" {
+  description = "TTL in seconds"
+  type        = number
+  default     = 300
+}
+
+variable "records" {
+  description = "List of record values"
+  type        = list(string)
+}
