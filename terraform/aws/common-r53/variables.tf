a04ecd5e59407a37b1c961eec4e2729e1c9b33a7
Yukiya Hayashi A.k.a morihaya <yukiya.hayashi@gmail.com>
2026-01-02 13:57:26 +0900

    feat: Add unused variables for TFC global settings to silence warnings

---
+++ terraform/aws/common-r53/variables.tf
@@ -0,0 +1,28 @@
+# These variables are defined at the TFC organization level
+# but are not used in this workspace. Declaring them here
+# silences the "undeclared variable" warnings.
+#
+# Note: It's recommended to use TF_VAR_... environment variables
+# for sensitive credentials like AWS_ACCESS_KEY_ID instead of
+# terraform.tfvars.
+
+variable "private_key" {
+  description = "Unused variable from TFC global settings"
+  type        = string
+  default     = ""
+  sensitive   = true
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "Unused variable from TFC global settings"
+  type        = string
+  default     = ""
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Unused variable from TFC global settings"
+  type        = string
+  default     = ""
+  sensitive   = true
+}
