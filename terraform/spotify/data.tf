91a56a47773788d780e0fa26b42892b5a2e1a376
Yukiya Hayashi A.k.a morihaya <yukiya.hayashi@gmail.com>
2026-01-04 22:44:07 +0900

    fix: terraform format album entries for consistency in Spotify data source

--- terraform/spotify/data.tf
+++ terraform/spotify/data.tf
@@ -2,41 +2,41 @@
 
 data "spotify_search_track" "BOC_01" {
   artist = "BUMP OF CHICKEN"
-  album = "FLAME VEIN +1"
+  album  = "FLAME VEIN +1"
 }
 data "spotify_search_track" "BOC_02" {
   artist = "BUMP OF CHICKEN"
-  album = "THE LIVING DEAD"
+  album  = "THE LIVING DEAD"
 }
 data "spotify_search_track" "BOC_03" {
   artist = "BUMP OF CHICKEN"
-  album = "jupiter"
+  album  = "jupiter"
 }
 data "spotify_search_track" "BOC_04" {
   artist = "BUMP OF CHICKEN"
-  album = "ユグドラシル"
+  album  = "ユグドラシル"
 }
 data "spotify_search_track" "BOC_05" {
   artist = "BUMP OF CHICKEN"
-  album = "orbital period"
+  album  = "orbital period"
 }
 
 # RIP SLYME
 data "spotify_search_track" "RIP_02" {
   artist = "RIP SLYME"
-  album = "FIVE"
+  album  = "FIVE"
 }
 data "spotify_search_track" "RIP_03" {
   artist = "RIP SLYME"
-  album = "TOKYO CLASSIC"
+  album  = "TOKYO CLASSIC"
 }
 
 # Ozawa Kenji
 data "spotify_search_track" "OzaKen_00" {
   artist = "Ozawa Kenji"
-  album = "LIFE"
+  album  = "LIFE"
 }
 data "spotify_search_track" "OzaKen_02" {
   artist = "Ozawa Kenji"
-  album = "刹那"
+  album  = "刹那"
 }
