c7d3abf544bb5a4ead538df1673cccad51d550cb
Yukiya Hayashi A.k.a morihaya <yukiya.hayashi@gmail.com>
2026-01-04 14:56:38 +0900

    feat: Add Japan East resource group to Azure configuration

--- terraform/azure/resource_groups.tf
+++ terraform/azure/resource_groups.tf
@@ -3,3 +3,9 @@ resource "azurerm_resource_group" "eastus" {
   location = "East US"
   tags     = local.common_tags
 }
+
+resource "azurerm_resource_group" "japaneast" {
+  name     = "morihaya-infra-rg-japaneast"
+  location = "Japan East"
+  tags     = local.common_tags
+}
