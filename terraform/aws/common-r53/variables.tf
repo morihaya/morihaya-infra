6f9ef11bffb1806ea568761ec89dc4691bdde17c
Yukiya Hayashi A.k.a morihaya <yukiya.hayashi@gmail.com>
2026-01-04 19:23:31 +0900

    refactor: add aws_accountid variable to silence undeclared variable warnings

--- terraform/aws/common-r53/variables.tf
+++ terraform/aws/common-r53/variables.tf
@@ -5,6 +5,11 @@
 # Note: It's recommended to use TF_VAR_... environment variables
 # for sensitive credentials like AWS_ACCESS_KEY_ID instead of
 # terraform.tfvars.
+variable "aws_accountid" {
+  description = "Unused variable from TFC global settings"
+  type        = string
+  default     = ""
+}
 
 variable "private_key" {
   description = "Unused variable from TFC global settings"
