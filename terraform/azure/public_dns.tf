7d84d2c8347f185a432be9b865a7bdb663918388
Yukiya Hayashi A.k.a morihaya <yukiya.hayashi@gmail.com>
2026-01-04 15:35:32 +0900

    fix: change DMARC record type from A to TXT and update policy to 'none'

--- terraform/azure/public_dns.tf
+++ terraform/azure/public_dns.tf
@@ -5,11 +5,13 @@ resource "azurerm_dns_zone" "azure-morihaya-tech" {
   tags = local.common_tags
 }
 
-resource "azurerm_dns_a_record" "dmarc" {
+resource "azurerm_dns_txt_record" "dmarc" {
   name                = "_dmarc"
   zone_name           = azurerm_dns_zone.azure-morihaya-tech.name
   resource_group_name = azurerm_resource_group.eastus.name
   ttl                 = 3600
-  records             = ["v=DMARC1;p=reject"]
-  tags                = local.common_tags
+  record {
+    value = "v=DMARC1; p=none"
+  }
+  tags = local.common_tags
 }
