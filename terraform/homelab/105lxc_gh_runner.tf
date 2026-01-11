ed0b42985f0d8c7109e86d776e06a37335cea1fe
Yukiya Hayashi A.k.a morihaya <yukiya.hayashi@gmail.com>
2026-01-11 14:02:08 +0900

    add: create LXC Container 105 for GitHub Actions Self-hosted Runner

---
+++ terraform/homelab/105lxc_gh_runner.tf
@@ -0,0 +1,78 @@
+# =============================================================================
+# LXC Container 105 - GitHub Actions Self-hosted Runner
+# =============================================================================
+resource "proxmox_virtual_environment_container" "lxc_105" {
+  node_name   = var.proxmox_node_name
+  vm_id       = 105
+  description = "GitHub Actions Self-hosted Runner - Managed by Terraform"
+
+  unprivileged = true
+  started      = true
+
+  features {
+    nesting = true
+  }
+
+  console {
+    enabled   = true
+    tty_count = 2
+    type      = "tty"
+  }
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+    swap      = 512
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 20
+  }
+
+  network_interface {
+    name     = "eth0"
+    bridge   = "vmbr0"
+    firewall = true
+  }
+
+  initialization {
+    hostname = "gh-runner"
+
+    ip_config {
+      ipv4 {
+        address = "192.168.1.9/24"
+        gateway = "192.168.1.1"
+      }
+    }
+
+    user_account {
+      keys = [
+        trimspace(var.morihaya_ssh_public_key)
+      ]
+      password = var.PROXMOX_DEFAULT_PASSWORD
+    }
+
+  }
+
+  operating_system {
+    template_file_id = "local:vztmpl/debian-12-standard_12.12-1_amd64.tar.zst"
+    type             = "debian"
+  }
+
+  startup {
+    down_delay = -1
+    order      = 99
+    up_delay   = -1
+  }
+
+  lifecycle {
+    ignore_changes = [
+      initialization,
+      operating_system,
+    ]
+  }
+}
