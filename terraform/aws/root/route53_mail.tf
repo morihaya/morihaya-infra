3e1be810022e5013ed4067abdb284cf1d4acfdac
Yukiya Hayashi A.k.a morihaya <yukiya.hayashi@gmail.com>
2026-01-12 17:10:36 +0900

    fix: update IP address and DKIM records in route53_mail.tf for email configuration

--- terraform/aws/root/route53_mail.tf
+++ terraform/aws/root/route53_mail.tf
@@ -32,7 +32,7 @@ resource "aws_route53_record" "mail_a" {
   type    = "A"
   ttl     = 300
   records = [
-    "106.73.59.225" # Dynamic IP
+    "34.169.244.99" # Public IP
   ]
 }
 
@@ -54,12 +54,13 @@ resource "aws_route53_record" "dkim_1" {
   type    = "TXT"
   ttl     = 300
   records = [
-    "v=DKIM1; k=ed25519; h=sha256; p=KJE3gVewwPZd9dUzb6meTbEscgQjRKZSw3XRKCX5+Zk="
+    "v=DKIM1; k=ed25519; h=sha256; p=UiU34pbHXcp8jeHIDomrsdadAthCH7AvFoHLPclmHJE="
   ]
 }
 
+# Route53の仕様で255文字を超えるTXTレコードは分割して登録する必要があるため、localで分割したものを結合して登録する。
 locals {
-  dkim2_value = "v=DKIM1; k=rsa; h=sha256; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzcO4IIjzKwHGAZvVPfzxqWDSQ/Tgy3+lgqc5wh8OEnq5ne3MweVLawxBLIbfXn4mALfr899l/pO35XsR0xQM3LxGJ5H0m/z6V/I/FDKHGCWJLG7n0faXk6rlejDrx+Roz25C3cKqxJulKzhEtJbiYiDxxVZPi7jS0X6Si9I/1NlLyDTNO9eo8Mrh+PYh0hcikfzdwg8sRXNITpBycvv85mHaY1k+j4+NwLCPvJMvtUJOnzXXNLYafxNJZ0aP3zI/Oun8gmnPojUwndwH8aXq/lSLuMKLjhsmvK8HTfNgc2YjFAh2jtW8SDRIbie4oojdc69Dgv68M+LA1kVQ4zdszwIDAQAB"
+  dkim2_value = "v=DKIM1; k=rsa; h=sha256; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwVcAQOH0Uaq/my3mSoBfTsKdfaNuIdRoxYqUzzg79onMx9fO/69uzYeBEcFa+SoE3j04Z2PJHlimK71QECIe5QKXyag16fOtUz8PPy2ZVIXi4JKP4ZJlrouZ0FPvWeC/wNV1eWvvKGqodTwV9375C0ktZcQ8AMEu/O2GWelfyQ6szWQVVnrzXgqvWwEuyWSQsrmXRoP0trOh1EAw9qDsNRe6ToBOyCxso8OGyhabLtEcYT3NEn7xrYA714hbkWGBkJabjOBEMOJLcqrKJcsQvxgKX9Nwiq0KFjIRGnEpX5qs6Uijea8fQjPNswLXpyiC2w10/Hah6mlpLnaSN0bYuwIDAQAB"
 }
 resource "aws_route53_record" "dkim_2" {
   zone_id = aws_route53_zone.morihaya-tech.zone_id
